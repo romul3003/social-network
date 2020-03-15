91c35f556dbc1a21faeb8f1804694b12292ce61c
Roman Zakrevskyi <romul3003@gmail.com>
2020-03-15 14:55:04 +0200

    Refactor MyPosts, convert to class component, add shouldComponentUpdete lifecycle  method

--- src/components/Profile/MyPosts/MyPosts.js
+++ src/components/Profile/MyPosts/MyPosts.js
@@ -3,24 +3,30 @@ import classes from './MyPosts.module.css'
 import Post from './Post/Post'
 import AddNewPostForm from './AddNewPostForm/AddNewPostForm'
 
-const MyPosts = props => {
-	const postElements = props.posts.map(post => (
-		<Post message={post.message} likesCount={post.likesCount} key={post.id} />
-	))
-
-	const onAddPost = values => {
-		props.addPost(values.newPostText)
+class MyPosts extends React.Component {
+	shouldComponentUpdate(nextProps, nextState, nextContext) {
+		return nextProps !== this.props || nextState !== this.state
 	}
 
-	return (
-		<div className={classes.postsBlock}>
-			<h3>My posts</h3>
-			<div>
-				<AddNewPostForm onSubmit={onAddPost} />
+	render() {
+		const postElements = this.props.posts.map(post => (
+			<Post message={post.message} likesCount={post.likesCount} key={post.id} />
+		))
+
+		const onAddPost = values => {
+			this.props.addPost(values.newPostText)
+		}
+
+		return (
+			<div className={classes.postsBlock}>
+				<h3>My posts</h3>
+				<div>
+					<AddNewPostForm onSubmit={onAddPost} />
+				</div>
+				<div className={classes.posts}>{postElements}</div>
 			</div>
-			<div className={classes.posts}>{postElements}</div>
-		</div>
-	)
+		)
+	}
 }
 
 export default MyPosts
