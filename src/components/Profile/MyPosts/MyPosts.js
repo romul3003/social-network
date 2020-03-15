606cf33de9914ca62dbf7d3b1c81cf48f324924d
Roman Zakrevskyi <romul3003@gmail.com>
2020-03-15 15:06:10 +0200

    Refactor MyPosts, add React.memo

--- src/components/Profile/MyPosts/MyPosts.js
+++ src/components/Profile/MyPosts/MyPosts.js
@@ -3,30 +3,24 @@ import classes from './MyPosts.module.css'
 import Post from './Post/Post'
 import AddNewPostForm from './AddNewPostForm/AddNewPostForm'
 
-class MyPosts extends React.PureComponent {
-	// shouldComponentUpdate(nextProps, nextState, nextContext) {
-	// 	return nextProps !== this.props || nextState !== this.state
-	// }
+const MyPosts = React.memo(props => {
+	const postElements = props.posts.map(post => (
+		<Post message={post.message} likesCount={post.likesCount} key={post.id} />
+	))
 
-	render() {
-		const postElements = this.props.posts.map(post => (
-			<Post message={post.message} likesCount={post.likesCount} key={post.id} />
-		))
-
-		const onAddPost = values => {
-			this.props.addPost(values.newPostText)
-		}
-
-		return (
-			<div className={classes.postsBlock}>
-				<h3>My posts</h3>
-				<div>
-					<AddNewPostForm onSubmit={onAddPost} />
-				</div>
-				<div className={classes.posts}>{postElements}</div>
-			</div>
-		)
+	const onAddPost = values => {
+		props.addPost(values.newPostText)
 	}
-}
+
+	return (
+		<div className={classes.postsBlock}>
+			<h3>My posts</h3>
+			<div>
+				<AddNewPostForm onSubmit={onAddPost} />
+			</div>
+			<div className={classes.posts}>{postElements}</div>
+		</div>
+	)
+})
 
 export default MyPosts
