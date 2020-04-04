6f9f279bf419a9e699355ed2a1029eb3af6455f0
Roman Zakrevskyi <romul3003@gmail.com>
2020-04-04 20:32:50 +0300

    Make appReducer with types

--- src/redux/appRecucer.ts
+++ src/redux/appRecucer.ts
@@ -2,11 +2,15 @@ import { getAuthUserData } from './authReducer'
 
 const INITIALIZED_SUCCESS = 'social-network/app/INITIALIZED_SUCCESS'
 
-const initialState = {
+export type InitialStateType = {
+	initialized: boolean
+}
+
+const initialState: InitialStateType = {
 	initialized: false,
 }
 
-const appReducer = (state = initialState, action) => {
+const appReducer = (state = initialState, action: any): InitialStateType => {
 	switch (action.type) {
 		case INITIALIZED_SUCCESS:
 			return {
@@ -18,13 +22,17 @@ const appReducer = (state = initialState, action) => {
 	}
 }
 
+type InitializedSuccessActionType = {
+	type: typeof INITIALIZED_SUCCESS
+}
+
 // action creators
-export const initializedSuccess = () => ({
+export const initializedSuccess = (): InitializedSuccessActionType => ({
 	type: INITIALIZED_SUCCESS,
 })
 
 // thunk creators
-export const initializeApp = () => dispatch => {
+export const initializeApp = () => (dispatch: any) => {
 	const promise = dispatch(getAuthUserData())
 
 	Promise.all([promise]).then(() => {
